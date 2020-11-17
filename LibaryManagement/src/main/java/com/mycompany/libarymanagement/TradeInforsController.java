cecfd6985df80c2416a6cc95e37d2390e7cf7f3a
DanteTran1710 <1851010125thanh@ou.edu.vn>
2020-11-17 23:59:08 +0700

    update Index view & action chuyen views

--- LibaryManagement/src/main/java/com/mycompany/libarymanagement/TradeInforsController.java
+++ LibaryManagement/src/main/java/com/mycompany/libarymanagement/TradeInforsController.java
@@ -5,10 +5,80 @@
  */
 package com.mycompany.libarymanagement;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author hp
  */
-public class TradeInforsController {
+public class TradeInforsController implements Initializable{
+    // Borrow Book
+    @FXML TextField name;
+    @FXML TextField phoneNumber;
+    @FXML ComboBox candidate;
+    @FXML Spinner bookCounted;
+    @FXML DatePicker returnDay;
+    @FXML TextField bookName;
+    @FXML TextField stateBook;
+    @FXML TextField cardName;
+    @FXML TextField stateCard;
+    @FXML DatePicker borrowDay;
     
+    // Returning Book
+    @FXML TextField nameCus;
+    @FXML TextField idCus;
+    @FXML ComboBox candidate1;
+    @FXML DatePicker borrowDay1;
+    @FXML Spinner bookCounted1;
+    @FXML CheckBox tornBookChk;
+    @FXML CheckBox stolenBookChk;
+    @FXML Spinner tornBookCounted;
+    @FXML Spinner stolenBookCounted;
+    
+    @FXML 
+    public void swtichToIndex() throws IOException
+    {
+        App.setRoot("Index");
+    }
+    
+    @FXML
+    public void switchtoChoice() throws IOException
+    {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Choose to choice");
+        alert.setContentText("Your returning-fill informations is done!/n"
+                + "Do you want to see your bills?");
+        
+        ButtonType btnYes = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+        ButtonType btnNo = new ButtonType("No", ButtonBar.ButtonData.NO);
+        
+        alert.getButtonTypes().setAll(btnNo,btnYes);
+        Optional<ButtonType> result = alert.showAndWait();
+        
+        if(result.get() == btnYes)
+        {
+            // Show the total bills
+             App.setRoot("TradeInfors");
+        }
+        else
+            App.setRoot("Index");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
 }
