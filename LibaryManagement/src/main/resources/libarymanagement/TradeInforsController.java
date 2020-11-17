c72e265b1c6806a66756117413bb0af523f5b16a
DanteTran1710 <1851010125thanh@ou.edu.vn>
2020-11-17 14:10:32 +0700

    Sua loi project (except Search View)

--- LibaryManagement/src/main/resources/libarymanagement/TradeInforsController.java
+++ LibaryManagement/src/main/resources/libarymanagement/TradeInforsController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.libarymanagement;
+package libarymanagement;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,7 +14,7 @@ import javafx.fxml.Initializable;
  *
  * @author hp
  */
-public class BaoCaoThongKeController implements Initializable {
+public class TradeInforsController implements Initializable {
 
     /**
      * Initializes the controller class.
