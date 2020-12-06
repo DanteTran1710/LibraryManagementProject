6b2236591c4497a9908c1e63667c6217aa6f4a4b
lathang909 <42415748+lathang909@users.noreply.github.com>
2020-12-06 22:05:02 +0700

    hoan thanh Search

--- LibraryManagement/src/main/java/com/mycompany/librarymanagement/IndexController.java
+++ LibraryManagement/src/main/java/com/mycompany/librarymanagement/IndexController.java
@@ -1,10 +1,5 @@
 package com.mycompany.librarymanagement;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.io.IOException;
 import java.net.URL;
